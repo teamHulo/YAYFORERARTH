bc62dbe0388ce3a9efad77440e3082ab783db55e
Aleksey <alenrouse123@gmail.com>
2023-11-03 00:13:25 +0200

    home all sections

---
+++ assets/a-script.js
@@ -0,0 +1,60 @@
+$(()=>{
+  let swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+    function handleResponse() {
+        let a = this.responseText;
+        console.log(a);
+        let parser = new DOMParser();
+        console.log(parser);
+        let objCarts = JSON.parse(this.responseText);
+        console.log(objCarts);
+        $("#cart-icon-bubble").html($(objCarts["cart-icon-bubble"]).html());
+        $("cart-drawer").html(
+          $(objCarts["cart-drawer"]).find("cart-drawer").html()
+        );
+      }
+
+    $(document).on('click',".add-my-cart", function (e) {
+        console.log('CLICK')
+        e.preventDefault();
+        let variant = $(this)
+          .closest(".my-product-card")
+          .find(".cart-id-first")
+          .val();
+        formData = {
+          id: variant,
+          quantity: 1,
+        };
+    
+        fetch(window.Shopify.routes.root + "cart/add.js", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        })
+          .then((response) => {
+            const request = new XMLHttpRequest();
+            request.addEventListener("load", handleResponse);
+            request.open(
+              "GET",
+              "?sections=cart-drawer,main-cart-items,cart-icon-bubble",
+              true
+            ); ///?sections=cart-drawer,
+            request.send();
+            $("cart-drawer")[0].open();
+            return response.json();
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          })
+          .finally(function () {
+            $("cart-drawer").removeClass("is-empty");
+          });
+      });
+})
